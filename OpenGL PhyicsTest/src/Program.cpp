3bcfcb3a148478053c4c064ad3ca650210aec7a1
Kacper Mazur <kacper.mazur888@gmail.com>
2019-12-24 05:36:25 +0000

    call back in loop

--- OpenGL PhyicsTest/src/Program.cpp
+++ OpenGL PhyicsTest/src/Program.cpp
@@ -9,7 +9,7 @@
 #include "Application.h"
 #include "ResoruceManager.h"
 
-void InputCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 const unsigned int SCREEN_WIDTH = 800;
 const unsigned int SCREEN_HEIGHT = 600;
@@ -18,85 +18,94 @@ Application PhysicsCollisonsApp(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 int main(void)
 {
-	GLFWwindow* window;
-
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Physics OpenGL", NULL, NULL);
+	
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Physics OpenGL", NULL, NULL);
 	if (!window)
-	{
+	{void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
 
 	glewExperimental = GL_TRUE;
-	
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error: GLEW didnt initilize" << std::endl;
-	
 	glGetError();
-
-	glfwSetKeyCallback(window, InputCallback);
-	
-	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	PhysicsCollisonsApp.Initialize();
-	
-	std::cout << glGetString(GL_VERSION) << std::endl;
-
-	ImGui::CreateContext();
-	ImGui_ImplGlfwGL3_Init(window, true);
-	ImGui::StyleColorsDark();
-
-	float deltaTime = 0.0f;
-	float lastFrame = 0.0f;
-
-	PhysicsCollisonsApp.m_state = ACTIVE;
-	
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
 	{
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		glfwPollEvents();
 
-		PhysicsCollisonsApp.InputHandler(deltaTime);
-		PhysicsCollisonsApp.Update(deltaTime);
-		
-		/* Render here */
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		PhysicsCollisonsApp.Render();
-		
-		ImGui_ImplGlfwGL3_NewFrame();
+		glfwSetKeyCallback(window, key_callback);
 
+		glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+		glFrontFace(GL_CW);
+		glCullFace(GL_FRONT);
+		glEnable(GL_CULL_FACE);
+
+		PhysicsCollisonsApp.Initialize();
+
+		std::cout << glGetString(GL_VERSION) << std::endl;
+
+
+		float deltaTime = 0.0f;
+		float lastFrame = 0.0f;
+
+		PhysicsCollisonsApp.m_state = ACTIVE;
+
+		glm::vec3 testValue(0.0f, 0.0f, 0.0f);
+
+		ImGui::CreateContext();
+		ImGui_ImplGlfwGL3_Init(window, true);
+		ImGui::StyleColorsDark();
+		
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
 		{
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			float currentFrame = glfwGetTime();
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+
+			glfwSetKeyCallback(window, key_callback);
+
+			PhysicsCollisonsApp.InputHandler(deltaTime);
+			PhysicsCollisonsApp.Update(deltaTime);
+
+			/* Render here */
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			
+			ImGui_ImplGlfwGL3_NewFrame();
+			
+			PhysicsCollisonsApp.Render();
+
+			{
+				ImGui::SliderFloat3("Rotation", &testValue.x, -1.0f, 2.0f);
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
+			
+
+			ImGui::Render();
+			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+			
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
-
-
-		ImGui::Render();
-		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
 	}
 
 	ResourceManager::Clear();
 	
 	ImGui_ImplGlfwGL3_Shutdown();
 	ImGui::DestroyContext();
+	
 	glfwTerminate();
 	return 0;
 }
 
-void InputCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 	{
@@ -104,6 +113,11 @@ void InputCallback(GLFWwindow* window, int key, int scancode, int action, int mo
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 
+	if (key == GLFW_KEY_1 && action == GLFW_PRESS)
+	{
+		std::cout << "1 is pressed" << std::endl;
+	}
+	
 	if(key >= 0 && key < 1024)
 	{
 		if (action == GLFW_PRESS)
