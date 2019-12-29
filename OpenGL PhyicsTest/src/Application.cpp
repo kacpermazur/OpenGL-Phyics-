24c07ca5fa749bebf740de8231777f3ff1cb7b0f
Kacper Mazur <kacper.mazur888@gmail.com>
2019-12-29 05:53:02 +0000

    OnTriggerEnter2D

--- OpenGL PhyicsTest/src/Application.cpp
+++ OpenGL PhyicsTest/src/Application.cpp
@@ -9,9 +9,22 @@
 
 #include <iostream>
 
+//todo: Add Entities Transfrom & Collider
+
+// AABB Collisions
+bool OnEnterTrigger2D(GameObject& target, GameObject& other)
+{
+	bool onCollisonX = target.m_position.x + target.m_size.x >= other.m_position.x && other.m_position.x + other.m_size.x >= target.m_position.x;
+	
+	bool onCollisonY = target.m_position.y + target.m_size.y >= other.m_position.y && other.m_position.y + other.m_size.y >= target.m_position.y;
+
+	return onCollisonX && onCollisonY;
+}
+
 // App State Data
 SpriteRenderer* renderer;
 GameObject* player;
+GameObject* testObj;
 
 Application::Application(const unsigned width, const unsigned height)
 	: m_width(width), m_height(height), m_input(), m_state(ACTIVE)
@@ -38,11 +51,13 @@ void Application::Initialize()
 
 	// Texture loading
 	ResourceManager::LoadTexture("res/img/redsonic.png", GL_TRUE, "sonic");
+	ResourceManager::LoadTexture("res/img/pepe.png", GL_TRUE, "pepe");
 
 	// Renderer ToDO: Renderer Class Agnostic (2D/3D) 
 	renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
 
-	player = new GameObject(glm::vec3(200, 200,0),glm::vec3(200, 200,1), ResourceManager::GetTexture("sonic"));
+	player = new GameObject(glm::vec3(300, 300,0),glm::vec3(100, 100,1), ResourceManager::GetTexture("sonic"));
+	testObj = new GameObject(glm::vec3(100, 100, 0), glm::vec3(100, 100, 1), ResourceManager::GetTexture("pepe"));
 }
 
 void Application::InputHandler(float& delta)
@@ -55,19 +70,19 @@ void Application::InputHandler(float& delta)
 
 		if(this->m_input[GLFW_KEY_W])
 		{
-			player->m_position.y += movementSpeed;
+			player->m_position.y -= movementSpeed;
 		}
 		if (this->m_input[GLFW_KEY_S])
 		{
-			player->m_position.y -= movementSpeed;
+			player->m_position.y += movementSpeed;
 		}
 		if (this->m_input[GLFW_KEY_D])
 		{
-			player->m_position.y += movementSpeed;
+			player->m_position.x += movementSpeed;
 		}
 		if (this->m_input[GLFW_KEY_A])
 		{
-
+			player->m_position.x -= movementSpeed;
 		}
 		
 	}
@@ -80,19 +95,15 @@ void Application::Update(float& delta)
 
 void Application::PhysicsUpdate()
 {
-	
+	if(OnEnterTrigger2D(*player, *testObj))
+	{
+		std::cout << "2D collison has occured!" << std::endl;
+	}
 }
 
 void Application::Render()
 {
 	
 	player->Draw(*renderer);
-}
-
-//todo: Add Entities Transfrom & Collider
-
-// AABB Collisions
-static bool OnEnterTrigger2D(GameObject& target, GameObject& other)
-{
-	return true;
+	testObj->Draw(*renderer);
 }
