5f2c0ce6e96ca37859373bfa9fe15d2b069c21e0
Kacper Mazur <kacper.mazur888@gmail.com>
2019-12-29 06:56:47 +0000

    vertex layout implemented

---
+++ OpenGL PhyicsTest/src/VertexLayout.cpp
@@ -0,0 +1,38 @@
+#include "VertexLayout.h"
+
+VertexLayout::VertexLayout()
+	:m_Stride(0)
+{
+}
+
+template <typename T>
+void VertexLayout::Add(unsigned count)
+{
+	static_assert(false, "Unsupported Data Type");
+}
+
+template <>
+void VertexLayout::Add<float>(unsigned count)
+{
+	m_Elements.push_back({ GL_FLOAT,count,GL_FALSE });
+
+	m_Stride += VertexElement::TypeSize(GL_FLOAT) * count;
+}
+
+template <>
+void VertexLayout::Add<unsigned int>(unsigned count)
+{
+	m_Elements.push_back({ GL_UNSIGNED_INT,count,GL_FALSE });
+
+	m_Stride += VertexElement::TypeSize(GL_UNSIGNED_INT) * count;
+}
+
+const std::vector<VertexElement> VertexLayout::GetElements() const
+{
+	return m_Elements;
+}
+
+unsigned VertexLayout::GetStride() const
+{
+	return m_Stride;
+}
